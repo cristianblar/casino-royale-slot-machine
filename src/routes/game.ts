69b48dc4b2bca96f6201b4a416f9cb083b7c03cd
Cristian Blandon <cristian@cristianblar.dev>
2022-01-30 22:08:04 -0500

    refactor: adapt game Router to real game logic

--- src/routes/game.ts
+++ src/routes/game.ts
@@ -1,10 +1,25 @@
 import { ensureAuthentication } from '../middlewares'
+import { gameService, userService } from '../services'
 import { Router } from 'express'
 
 const gameRouter = Router()
 
 gameRouter.get('/play', ensureAuthentication, (req, res) => {
-  res.sendStatus(200)
+  if (req.session.session_credits === 0) return res.sendStatus(402)
+
+  req.session.session_credits -= 1
+  const { isWinner, result } = gameService.play(req.session.session_credits)
+
+  if (!isWinner) {
+    if (req.session.session_credits === 0)
+      userService.cashOutCredits(req.user!.id, 0)
+    return res
+      .status(200)
+      .json({ credits: req.session.session_credits, result })
+  }
+
+  req.session.session_credits += result[0].score
+  return res.status(200).json({ credits: req.session.session_credits, result })
 })
 
 export default gameRouter
