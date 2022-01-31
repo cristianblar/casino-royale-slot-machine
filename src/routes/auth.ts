eb6237d5156bc83071381eb6d496268808de04e2
Cristian Blandon <cristian@cristianblar.dev>
2022-01-30 22:08:15 -0500

    refactor: adapt auth Router for real game logic

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,8 @@
-import { Router } from 'express'
-import { passport } from '../auth'
-import { userService } from '../services'
 import { ensureAuthentication } from '../middlewares'
+import { passport } from '../auth'
+import { Router } from 'express'
+import { symbols } from '../constants'
+import { userService } from '../services'
 
 const authRouter = Router()
 
@@ -12,14 +13,20 @@ authRouter.post('/signup-signin', (req, res, next) => {
     req.login(user, error => {
       if (error) return res.status(400).json({ error })
       req.session.session_credits = req.user!.credits
-      return res.sendStatus(200)
+      return res
+        .status(200)
+        .json({ credits: req.session.session_credits, symbols })
     })
   })(req, res, next)
 })
 
 authRouter.post('/logout', ensureAuthentication, async (req, res) => {
   try {
-    await userService.cashOutCredits(req.user!.id, req.session.session_credits)
+    if (req.user!.credits !== req.session.session_credits)
+      await userService.cashOutCredits(
+        req.user!.id,
+        req.session.session_credits
+      )
     req.logout()
     return res.sendStatus(200)
   } catch (error) {
